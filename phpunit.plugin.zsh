7046206844761fb6452505ccfd272fcce551a5ca
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2024-03-06 21:17:02 +0300

    Fix CS

--- phpunit.plugin.zsh
+++ phpunit.plugin.zsh
@@ -1,22 +1,22 @@
-alias pu='phpunit_cmd';
-alias puinit='phpunit_cmd --generate-configuration';
+alias pu='phpunit_cmd'
+alias puinit='phpunit_cmd --generate-configuration'
 
 phpunit_cmd() {
-    $(__phpunit_cmd) "$@";
+    $(__phpunit_cmd) "$@"
 }
 
 # Generates phpunit command
 __phpunit_cmd() {
-    local -r project_dir="$(__phpunit_project_dir)";
-    local -r phpunit_bin="$(__phpunit_bin ${project_dir})";
-    local -r phpunit_config_dir="$(__phpunit_config_dir ${project_dir})";
+    local -r project_dir="$(__phpunit_project_dir)"
+    local -r phpunit_bin="$(__phpunit_bin ${project_dir})"
+    local -r phpunit_config_dir="$(__phpunit_config_dir ${project_dir})"
 
-    echo "${phpunit_bin} ${phpunit_config_dir:+-c} ${phpunit_config_dir}";
+    echo "${phpunit_bin} ${phpunit_config_dir:+-c} ${phpunit_config_dir}"
 }
 
 # Finds phpunit executable
 __phpunit_bin() {
-    local -r project_dir="${1:-$(__phpunit_project_dir)}";
+    local -r project_dir="${1:-$(__phpunit_project_dir)}"
 
     local dir
     for dir in "${project_dir}/"{bin,tools,vendor/bin,}; do
@@ -31,96 +31,96 @@ __phpunit_bin() {
 
 # Finds project dir
 __phpunit_project_dir() {
-    local project_dir="${PWD}";
-    local dir="${project_dir}";
+    local project_dir="${PWD}"
+    local dir="${project_dir}"
 
     while ((1)); do
         if [[ -f "${dir}/composer.json" ]]; then
-            project_dir="${dir}";
-            break;
+            project_dir="${dir}"
+            break
         fi
 
-        [[ -z "${dir}" ]] && break;
+        [[ -z "${dir}" ]] && break
 
-        dir="${dir%/*}";
+        dir="${dir%/*}"
     done
 
-    echo "${project_dir}";
+    echo "${project_dir}"
 }
 
 __phpunit_config_dir() {
-    local project_dir="${1:-$(__phpunit_project_dir)}";
-    local phpunit_config_file=$(find "$project_dir" -maxdepth 2 -name 'phpunit.xml*' -type f -printf "%d %p\n" 2>/dev/null | sort -n | cut -d " " -f 2 | head -n 1);
-    local phpunit_config_dir="${phpunit_config_file%/phpunit.xml*}";
+    local project_dir="${1:-$(__phpunit_project_dir)}"
+    local phpunit_config_file=$(find "$project_dir" -maxdepth 2 -name 'phpunit.xml*' -type f -printf "%d %p\n" 2>/dev/null | sort -n | cut -d " " -f 2 | head -n 1)
+    local phpunit_config_dir="${phpunit_config_file%/phpunit.xml*}"
 
-    echo "$phpunit_config_dir";
+    echo "${phpunit_config_dir}"
 }
 
 puwatch() {
     if ! which 'inotifywait' > /dev/null; then
-        echo 'Command "inotifywait" not found. Try to install the "inotify-tools" package.' >&2;
-        return 1;
+        echo 'Command "inotifywait" not found. Try to install the "inotify-tools" package.' >&2
+        return 1
     fi
 
-    local src_dir="${1%/}";
-    local test_dir="${2%/}";
+    local src_dir="${1%/}"
+    local test_dir="${2%/}"
 
-    if [[ -z "$src_dir" ]]; then
+    if [[ -z "${src_dir}" ]]; then
         if [[ -d './src' ]]; then
-            src_dir='./src';
+            src_dir='./src'
         else
-            src_dir='.';
+            src_dir='.'
         fi
     fi
 
-    if [[ -z "$test_dir" ]]; then
+    if [[ -z "${test_dir}" ]]; then
         if [[ -d 'tests' ]]; then
-            test_dir='./tests';
+            test_dir='./tests'
         elif [[ -d 'Tests'  ]]; then
-            test_dir='./Tests';
+            test_dir='./Tests'
         else
-            test_dir='.';
+            test_dir='.'
         fi
     fi
 
-    clear;
-    __puwatch_header "$src_dir" "$test_dir";
+    clear
+    __puwatch_header "${src_dir}" "${test_dir}"
 
     inotifywait -mre modify \
         --format '%w%f' \
         --exclude '(/\.|/vendor/|\[^.\]\[^p\]\[^h\]\[^p\]$)' \
-        "$src_dir" "$test_dir" | \
+        "${src_dir}" "${test_dir}" | \
 
     while read file; do
 
-        [[ "$file" == "$test_dir"* && ! "$test_dir" == "$src_dir" && ! "${file: -8}" == 'Test.php' ]] && continue;
+        [[ "${file}" == "${test_dir}"* && ! "${test_dir}" == "${src_dir}" && ! "${file: -8}" == 'Test.php' ]] && continue
 
-        local test_file="$file";
+        local test_file="${file}"
         if [[ ! "${file: -8}" == 'Test.php' ]]; then
-            test_file="${${file/$src_dir/$test_dir}//.php/Test.php}";
+            test_file="${${file/${src_dir}/${test_dir}}//.php/Test.php}"
         fi
 
-        clear;
-        __puwatch_header "$src_dir" "$test_dir";
+        clear
+        __puwatch_header "${src_dir}" "${test_dir}"
 
-        if [[ -f "$test_file" ]]; then
-            echo -e "\e[33m${test_file}\e[0m";
-            echo;
-            phpunit_cmd --colors --columns="$(tput cols)" "$test_file";
+        if [[ -f "${test_file}" ]]; then
+            echo -e "\e[33m${test_file}\e[0m"
+            echo
+            phpunit_cmd --colors --columns="$(tput cols)" "${test_file}"
         else
-            echo -e "\e[31mFile \"\e[91m${test_file}\e[31m\" not exists!\e[0m" >&2;
-            echo;
+            echo -e "\e[31mFile \"\e[91m${test_file}\e[31m\" not exists!\e[0m" >&2
+            echo
         fi
 
     done
 }
 
 __puwatch_header() {
-    echo "[$(date '+%F %H:%M:%S')]";
-    echo;
-    echo -e "\e[92mSources:\e[0m ${1}\e[0m";
-    echo -e "\e[92mTests:\e[0m   ${2}\e[0m";
-    echo;
+    echo "[$(date '+%F %H:%M:%S')]"
+    echo
+    echo -e "\e[92mSources:\e[0m ${1}\e[0m"
+    echo -e "\e[92mTests:\e[0m   ${2}\e[0m"
+    echo
 }
 
 compdef _gnu_generic phpunit_cmd
